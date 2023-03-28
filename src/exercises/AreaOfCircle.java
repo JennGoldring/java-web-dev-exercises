117ed79771f1edb40633d7e0878a8c193b1417bc
Jennifer <125608864+JennGoldring@users.noreply.github.com>
2023-03-27 19:59:56 -0500

    "New changes"

--- src/exercises/AreaOfCircle.java
+++
@@ -1,7 +0,0 @@
-package exercises
-import java.util.Scanner;
-public class AreaOfCircle {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -0,0 +1,12 @@
+package org.launchcode.java.studios.areaofacircle;
+import java.util.Scanner;
+public class Area {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter a radius: ");
+        double radius = input.nextDouble();
+//        double area = (3.14 * radius * radius);
+        double answer = Circle.getArea(radius);
+        System.out.println("The area of your circle is: " + answer);
+    }
+}
