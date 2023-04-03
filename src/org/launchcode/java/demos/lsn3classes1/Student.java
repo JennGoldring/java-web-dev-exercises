0ad7c15300034beef4e504b2c69e8656be23d875
Jennifer <125608864+JennGoldring@users.noreply.github.com>
2023-04-03 18:53:00 -0500

    Exercises Class 3

--- src/org/launchcode/java/demos/lsn3classes1/Student.java
+++ src/org/launchcode/java/demos/lsn3classes1/Student.java
@@ -4,10 +4,38 @@ package org.launchcode.java.demos.lsn3classes1;
 // To instantiate the Student class, add your code to the main in the file, SchoolPractice.
 
 public class Student {
-
     private String name;
     private int studentId;
     private int numberOfCredits = 0;
     private double gpa = 0.0;
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+    private void setNumberOfCredits(int numberOfCredits) {
+        this.numberOfCredits = numberOfCredits;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public int getNumberOfCredits() {
+        return numberOfCredits;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
 
 }
